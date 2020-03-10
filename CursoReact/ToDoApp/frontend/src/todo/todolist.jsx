28fa42b3b96c2fce68902da87f05d94a56628f0f
ReisAline <60517902+ReisAline@users.noreply.github.com>
2020-03-09 23:00:05 -0300

    estudando

--- CursoReact/ToDoApp/frontend/src/todo/todolist.jsx
+++ CursoReact/ToDoApp/frontend/src/todo/todolist.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import IconButon from '../template/iconButton'
 
-
-export default props =>(
-  <div>
-     <h1>List</h1>
-  </div>  
-)
+export default props =>{
+  const renderRows=()=>{
+    const list= props.list||[]
+    return list.map(todo =>(
+      <tr key={todo._id}>
+        <td className ={todo.done ? 'markedAsDone' : ''}>
+          {todo.description}
+        </td>
+        <td>
+          <IconButon style = 'success' icon = 'check' hide ={todo.done}
+                   onClick = {()=> props.handleMarkAsDone(todo)}/>
+          <IconButon style = 'warning' icon = 'undo'hide = {!todo.done}
+                   onClick = {()=> props.handleMarkAsPending(todo)}/>         
+          <IconButon style = 'danger' icon= 'trash-o'
+                   onClick = {()=> props.handleRemove(todo)}/>
+        </td>
+      </tr>
+    ))
+  }
+  return(
+    <table className= 'table'>
+      <thead>
+        <tr>
+          <th>Descricao</th>
+          <th className= 'tableActions' >Acoes</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+}
